2ea703f9420f85185acdcf38215e9e3d59e58ca6
handsomeGZXX <gzxx307@gmail.com>
2026-03-18 14:33:18 +0800

    [初始化] 创建新项目

---
+++ Assets/Scripts/Datas/Player.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Player
+{
+    public static string Name { get; set; }
+    public static int Ranks { get; set; }
+    
+}
