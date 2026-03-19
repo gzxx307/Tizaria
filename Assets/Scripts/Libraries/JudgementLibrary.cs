26b81b0be648d577ce07fd28bbdf302393497483
handsomeGZXX <gzxx307@gmail.com>
2026-03-19 18:46:58 +0800

    [重构] JudgementLibrary改为静态类

--- Assets/Scripts/Libraries/JudgementLibrary.cs
+++ Assets/Scripts/Libraries/JudgementLibrary.cs
@@ -2,30 +2,26 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JudgementLibrary
+public static class JudgementLibrary
 {
-    public JudgementLibrary Instance = new JudgementLibrary();
-
-    [SerializeField] private RankingRuleSO _rankingRuleSo;
-    [SerializeField] private JudgementTimeSO _judgementTimeSo;
-
     /// <summary>
     /// 根据配置表计算等级
     /// <param name="Score"> 分数 </param>
     /// <param name="NoteCount"> 物量 </param>
     /// <param name="IsFullCombo"> 是否为全连 </param>
     /// <param name="IsStrict"> 是否为严判 </param>
+    /// <param name="RankingRuleSo"> 结算等级配置SO </param>
     /// <returns> 评级 </returns>
     /// </summary>
-    public Rank GetRank(int Score, int NoteCount, bool IsFullCombo, bool IsStrict)
+    public static Rank GetRank(int Score, int NoteCount, bool IsFullCombo, bool IsStrict, RankingRuleSO RankingRuleSo)
     {
         // 如果不是严判，则不会到达理论值，直接将 NoteCount 置零以满足 T 级条件
         if (!IsStrict) NoteCount = 0; 
-        if (Score == _rankingRuleSo.TScore + NoteCount) return Rank.T;
-        if (Score >= _rankingRuleSo.SScore) return Rank.S;
-        if (Score >= _rankingRuleSo.AScore) return Rank.A;
-        if (Score >= _rankingRuleSo.BScore) return Rank.B;
-        if (Score >= _rankingRuleSo.CScore) return Rank.C;
+        if (Score == RankingRuleSo.TScore + NoteCount) return Rank.T;
+        if (Score >= RankingRuleSo.SScore) return Rank.S;
+        if (Score >= RankingRuleSo.AScore) return Rank.A;
+        if (Score >= RankingRuleSo.BScore) return Rank.B;
+        if (Score >= RankingRuleSo.CScore) return Rank.C;
         return Rank.F;
     }
 
@@ -33,14 +29,15 @@ public class JudgementLibrary
     /// 根据打击偏移计算打击结果
     /// </summary>
     /// <param name="DeltaMs"> 时间差（ms）</param>
+    /// <param name="JudgementTimeSo"> 打击判定范围配置SO </param>
     /// <returns> 打击结果 </returns>
-    public Judgement Evaluate(float DeltaMs)
+    public static Judgement Evaluate(float DeltaMs, JudgementTimeSO JudgementTimeSo)
     {
         float abs = Mathf.Abs(DeltaMs);
-        if (abs <= _judgementTimeSo.PerfectWindow) return Judgement.Perfect;
-        if (abs <= _judgementTimeSo.GreatWindow) return Judgement.Great;
-        if (abs <= _judgementTimeSo.GoodWindow) return Judgement.Good;
-        if (abs <= _judgementTimeSo.BadWindow) return Judgement.Bad;
+        if (abs <= JudgementTimeSo.PerfectWindow) return Judgement.Perfect;
+        if (abs <= JudgementTimeSo.GreatWindow) return Judgement.Great;
+        if (abs <= JudgementTimeSo.GoodWindow) return Judgement.Good;
+        if (abs <= JudgementTimeSo.BadWindow) return Judgement.Bad;
         // 如果超出判定时间范围则自动判定，并判定为Miss
         return Judgement.Miss;
     }
