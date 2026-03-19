84d65d54f34541b2fb644087869e263c7e26c77c
handsomeGZXX <gzxx307@gmail.com>
2026-03-19 19:24:42 +0800

    [删除] 删除MissWindow，超过BadWindow则判定为Miss

--- Assets/Scripts/Configs/JudgementTimeSO.cs
+++ Assets/Scripts/Configs/JudgementTimeSO.cs
@@ -13,6 +13,4 @@ public class JudgementTimeSO : ScriptableObject
     public int GoodWindow;
     // Bad 判定时间窗口（ms）
     public int BadWindow;
-    // Miss 判定时间窗口（ms），超过这个时间就算 Miss
-    public int MissWindow;
 }
